4d2083ec889c61092297723b150f8c4599c0aead
zied zahri <zied.realmadrid@gmail.com>
2024-04-03 11:35:52 +0200

    chekpoint

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Checkpoint5/*.{html,js}"
+             ,"./Checkpoint.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+}
+
